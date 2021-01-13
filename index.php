b43ab1b65148296f1bd9a7a321b6fede9c9587d5
Jonathan <jonathan.martinez@etu.univ-amu.fr>
2021-01-13 16:44:21 +0100

    test

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include 'utils.inc.php';
+start_page('VANESTARRE');
+?>
+
+<style>
+    body {
+        background-color: violet;
+    }
+</style>
+
+<?php
+echo '<img src="logo_large.png" alt="logo">'
+?>
+
+
+
+<?php
+end_page();
+?>
+
