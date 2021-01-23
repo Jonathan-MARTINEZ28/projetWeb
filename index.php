86906c04dc4330d396e30e86f37fd21cfdac8274
Jonathan <jonathan.martinez@etu.univ-amu.fr>
2021-01-23 19:41:39 +0100

    Test affichage de message

--- index.php
+++ index.php
@@ -3,17 +3,25 @@
     echo '
     <a href="Connection.php"><button>Se connecter</button></a><br>
     <a href="Inscription.php"><button>S\'inscrire</button></a><br>
-    <a href="déconnection.php"><button>déconnection</button></a><br>
-    <h3>message</h3><br>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p><br>
-    <select name="pays" id="pays">
-            <option value=""></option>
-            <option value="fr">love</option>
-            <option value="jp">cute</option>
-            <option value="us">trop stylé</option>
-            <option value="us">swag</option>
-        </select><br>
-    ';
+    <a href="déconnection.php"><button>déconnection</button></a><br>';
+
+    $dbLink = mysqli_connect('localhost', 'root', '')
+    or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+
+    mysqli_select_db($dbLink, 'vanestarretest')
+    or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+
+    require 'Modele/requestBD.php';
+
+    $query = "SELECT contenu, tag, date FROM messages";
+
+    getRequest($dbLink,$query);
+
+    $dbResult = mysqli_query($dbLink, $query);
+    $resultat = mysqli_fetch_array($dbResult);
+
+        echo $resultat['contenu'];
+
 
 $contenu = ob_get_clean();
     require 'View/gabarit.php';
