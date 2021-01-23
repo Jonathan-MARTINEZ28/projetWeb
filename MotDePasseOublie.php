7764be2c2571bd6c12c5628799a9472a7dc317a2
Jonathan <jonathan.martinez@etu.univ-amu.fr>
2021-01-23 18:17:20 +0100

    Changement de pseudo, d'adresse mail et de mot de passe

--- MotDePasseOublie.php
+++ MotDePasseOublie.php
@@ -1,12 +1,20 @@
 <?php
 ob_start();
 echo '
-    <form action="modele/gestionbd.php" method="post">
+    <form action="gestionbd.php" method="post">
         <label for="pseudo">Identifiant:</label><br>
         <input type="text" id="pseudo" name="pseudo"/><br>
+        
         <label for="mail">E-mail</label><br>
         <input type="text" id="mail" name="mail"/><br>
-        <input type="submit" value="Connection" name="action" id="recup" />
+        
+        <label for="newpass">Entrez votre nouveau mot de passe</label><br>
+        <input type="password" id="newpass" name="newpass"/><br>
+        
+        <label for="newpass2">Confirmez votre nouveau mot de passe</label><br>
+        <input type="password" id="newpass2" name="newpass2"/><br>
+        
+        <input type="submit" value="Changer de mot de passe" name="action" id="recup"/>
     </form><br>';
 $contenu = ob_get_clean();
 require 'vues/gabarit.php';
--- profil.php
+++ profil.php
@@ -5,7 +5,16 @@ if (isset($_SESSION['id']) AND isset($_SESSION['pseudo'])) {
     echo 'Pseudo: ' . $_SESSION['pseudo'] . '<br>' .
         'Adresse mail: ' . $_SESSION['mail'];
 }
-echo ' <input type="text" id="newpseudo" name="newpseudo"/><br>';
+echo ' <form action="gestionbd.php" method="post">
+        <label for="newpseudo">Changer de pseudo </label><br>
+        <input type="text" id="newpseudo" name="newpseudo"/>
+        <input type="submit" value="changer de pseudo" name="action" id="action" />
+    </form><br>
+    <form action="gestionbd.php" method="post">
+        <label for="newpseudo">changer d\'adresse mail</label><br>
+        <input type="text" id="newmail" name="newmail"/>
+        <input type="submit" value="changer d\'adresse mail" name="action" id="action" />
+    </form><br>';
 session_abort();
 $contenu = ob_get_clean();
 require 'vues/gabarit.php'
