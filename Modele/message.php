091acc5093e54ccf69f5bab88911ec4a2d913b84
Jonathan <jonathan.martinez@etu.univ-amu.fr>
2021-01-25 12:08:27 +0100

    début gestion des emoji

--- Modele/message.php
+++ Modele/message.php
@@ -1,10 +1,27 @@
 <?php
+require 'Modele/connectBD.php';
+require 'Modele/requestBD.php';
 
 function getMessage(){
-    $_message = $_POST['message'];
+    $dbLink = getBD();
+    $query = "SELECT id, contenu, tag, love, cute, tropstyle, swag date FROM messages";
+    getRequest($dbLink,$query);
+    $dbResult = mysqli_query($dbLink, $query);
+    return $dbResult;
 }
 
 function getSend(){
     $action_env = $_POST['envoyer'];
+}
+
+function affichermessage(){
+    $dbResult = getMessage();
+    while ($resultat = mysqli_fetch_array($dbResult)) {
+        $id = $resultat['id'];
+        echo $resultat['contenu'] . ' ' . 'β' .$resultat['tag'] . '<br>';
+        echo '<a href="../gestionemoji.php?t=love&id=<?= $id ?>"><button >love</button></a><br>';
+
+
+    }
 }
 
---
+++ gestionemoji.php
@@ -0,0 +1,7 @@
+<?php
+require 'Modele/message.php';
+$dblink = getBD();
+
+if(isset($_GET['t'],$_GET['id'])){
+
+}
